a2478a7fb2e056a0d76c8b5bb0595da4c007435b
Callum <mail@callumwong.com>
2026-04-25 22:51:40 +1000

    tidy up the nix flake

--- flake.nix
+++ flake.nix
@@ -1,61 +1,66 @@
 {
-  description = "homelab dashboard";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = self.lib.mkPrismTower {
-          inherit pkgs;
-          services = [ ];
-          links = [ ];
-          searchUrl = "";
+    { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              nodePackages.pnpm
+            ];
+          };
+
+          # Provide packages.default only to allow for `nix build` in CI.
+          # Because this project requires input depending on the configuration at build time,
+          # the NixOS config should use inputs.prism-tower.lib.mkPackage
+          # to build the package with access to the rest of the configuration.
+          packages.default = self.lib.mkPackage { inherit pkgs; };
         };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            nodejs
-            nodePackages.pnpm
-          ];
-        };
-      }
-    )
-    // {
-      lib.mkPrismTower =
+      flake.lib.mkPackage =
         {
           pkgs,
-          services,
-          links,
-          searchUrl,
+          services ? [ ],
+          links ? [ ],
+          searchUrl ? "",
+          ...
         }:
         pkgs.stdenv.mkDerivation (finalAttrs: {
           pname = "prism-tower";
-          version = "1.0.0";
+          version = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
           src = ./.;
 
           nativeBuildInputs = [
             pkgs.nodejs
             pkgs.pnpmConfigHook
-            pkgs.pnpm
+            pkgs.pnpm_10
           ];
 
+          # https://nixos.org/manual/nixpkgs/unstable/#javascript-pnpm
+          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/node/fetch-pnpm-deps/default.nix
+          pnpmInstallFlags = [ "--prod" ];
           pnpmDeps = pkgs.fetchPnpmDeps {
-            inherit (finalAttrs) pname version src;
             fetcherVersion = 3;
-            hash = "sha256-BKGzbkoG5Ua7xzdwsO05nSXqQovWjWcYqV8iE3p0zeQ=";
+            hash = "sha256-99SOCAGcQnMqOWyOnGDsTzZHtOh8ngVHmEE949H5KdQ=";
+            pnpm = pkgs.pnpm_10;
+            inherit (finalAttrs)
+              pname
+              version
+              src
+              pnpmInstallFlags
+              ;
+          };
+
+          env = {
+            SEARCH_URL = pkgs.lib.escapeShellArg searchUrl;
           };
 
           preBuild = ''
@@ -65,7 +70,6 @@
 
           buildPhase = ''
             runHook preBuild
-            export SEARCH_URL=${pkgs.lib.escapeShellArg searchUrl}
             pnpm build
             runHook postBuild
           '';
@@ -77,59 +81,5 @@
             runHook postInstall
           '';
         });
-
-      nixosModules.default =
-        { lib, ... }:
-        {
-          options.services.prism-tower = {
-            enable = lib.mkEnableOption "Prism Tower dashboard";
-
-            services = lib.mkOption {
-              description = "List of services to display in the dashboard";
-              default = [ ];
-              type = lib.types.listOf (
-                lib.types.submodule {
-                  options = {
-                    name = lib.mkOption {
-                      type = lib.types.str;
-                      example = "Jellyfin";
-                    };
-                    url = lib.mkOption {
-                      type = lib.types.str;
-                      example = "https://jellyfin.7sref";
-                    };
-                    iconUrl = lib.mkOption { type = lib.types.str; };
-                    category = lib.mkOption { type = lib.types.str; };
-                  };
-                }
-              );
-            };
-
-            links = lib.mkOption {
-              description = "Extra links that will appear in search autocomplete";
-              default = [ ];
-              type = lib.types.listOf (
-                lib.types.submodule {
-                  options = {
-                    name = lib.mkOption {
-                      type = lib.types.str;
-                      example = "ArchWiki";
-                    };
-                    url = lib.mkOption {
-                      type = lib.types.str;
-                      example = "https://wiki.archlinux.org";
-                    };
-                  };
-                }
-              );
-            };
-
-            searchUrl = lib.mkOption {
-              description = "URL for the search bar";
-              default = "https://google.com/search";
-              type = lib.types.str;
-            };
-          };
-        };
     };
 }
